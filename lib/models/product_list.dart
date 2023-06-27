de190101ba123601af641d69ac0e141fd593274e
Rodrigo Moura <rodrigo.joplin@gmail.com>
2023-06-27 17:49:40 -0300

    27_06_23 inserção firebase

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,11 +1,15 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/models/product.dart';
 
 import '../data/dummy_data.dart';
 
 class ProductList with ChangeNotifier {
+  final _baseUrl = 'https://shop-cod3r-75e28-default-rtdb.firebaseio.com/';
+
   List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
@@ -16,7 +20,7 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
-  void saveProduct(Map<String, Object> data) {
+  Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final product = Product(
@@ -28,24 +32,51 @@ class ProductList with ChangeNotifier {
     );
 
     if (hasId) {
-      updateProduct(product);
+      return updateProduct(product);
     } else {
-      addProduct(product);
+      return addProduct(product);
     }
   }
 
-  void addProduct(Product product) {
-    _items.add(product);
-    notifyListeners();
+  Future<void> addProduct(Product product) {
+    final future = http.post(
+      Uri.parse('$_baseUrl/products.json'),
+      body: jsonEncode(
+        {
+          "name": product.name,
+          "description": product.description,
+          "price": product.price,
+          "imageUrl": product.imageUrl,
+          "isFavorite": product.isFavorite,
+        },
+      ),
+    );
+
+    return future.then<void>(
+      (response) {
+        final id = jsonDecode(response.body)['name'];
+        _items.add(Product(
+          id: id,
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl,
+          isFavorite: product.isFavorite,
+        ));
+        notifyListeners();
+      },
+    );
   }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
+
+    return Future.value();
   }
 
   void removeProduct(Product product) {
